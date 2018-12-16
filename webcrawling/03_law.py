7a4563d99f7a741df8aa71031e6ae2a009e78fd2
ktjylsj <tjkimts@naver.com>
2018-12-16 21:19:09 +0900

    Update 03_law.py

--- webcrawling/03_law.py
+++ webcrawling/03_law.py
@@ -1,6 +1,6 @@
-import requests
 from selenium import webdriver as wd
 from bs4 import BeautifulSoup as bs
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 # 명시적 대기를 위해
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,42 +9,33 @@ import pymysql as my
 import time
 import sys
 
-main_url = 'http://likms.assembly.go.kr/law/lawsNormInqyMain1010.do?mappingId=%2FlawsNormInqyMain1010.do&genActiontypeCd=2ACT1010'
+main_url = 'http://lawnb.com/Info/ContentMain/Law'
 keyword = '동물'
 
-driver = wd.Chrome(executable_path='chromedriver.exe')
+driver: WebDriver = wd.Chrome(executable_path='chromedriver.exe')
 
 driver.get(main_url)
-driver.find_element_by_id('srchNm').send_keys(keyword)
-driver.find_element_by_css_selector('button.btn_search').click()
+driver.find_element_by_id('sWord').send_keys(keyword)
+driver.find_element_by_id('searchBtn').click()
 
 try:
     element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'tab_m'))
+        EC.presence_of_element_located((By.CLASS_NAME, 'submenu_group'))
     )
 except Exception as e:
     print('오류 발생', e)
 
-lawItems = driver.find_elements_by_id('searchResultLaw')
-links = []
-for li in lawItems:
-    list = len(li.find_elements_by_css_selector('a'))
-    for dt in range(list):
-        # print(li.find_elements_by_css_selector('a')[dt].get_attribute('href'))
-        links.append(li.find_elements_by_css_selector('a')[dt].get_attribute('href'))
-        # driver.execute_script(li.find_elements_by_css_selector('a')[dt].get_attribute('href'))
-        # time.sleep(2)
-    # print("텍스트: ", li.text)
+boxItems = driver.find_elements_by_css_selector('.main_container_inner')
+print(len(boxItems))
+print(boxItems.pop().text)
+boxItems = driver.find_elements_by_id('search_results')
+print(len(boxItems))
+print(boxItems.pop().text)
+# print(boxItems.find_elements_by_css_selector('li'))
+#for li in boxItems:
+#    print(li.text)
+#    time.sleep(2)
 
-max = len(links)
-for num in range(0, 1):
-    jScript = links.pop()
-    main_url = driver.execute_script(jScript)
-    driver
-    print(jScript)
-    print(main_url)
-    soup = bs(driver.page_source, 'html.parser')
-    # print(soup)
 # 종료
 driver.close()
 driver.quit()
