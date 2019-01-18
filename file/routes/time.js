0fd3c3d43284f2381afffd571ee024fc57ab52c5
ktjylsj <tjkimts@naver.com>
2019-01-18 11:26:13 +0900

    Create time.js

---
+++ file/routes/time.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'20150303jj',
+    database:'mydb'
+    //host:'39.120.184.198',
+    //user:'guest',
+    //password:'1q2w3e4r!',
+    //database:'my_db'
+});
+    connection.connect();
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var request = 'SELECT * FROM mydb.law_data WHERE title = "동물"';
+    connection.query(request, function(error, results){
+        if(error){
+            console.log(error);
+        }
+        else{
+            var test = JSON.stringify(results);
+            res.render('time', {test:test});
+        }
+    })
+});
+
+module.exports = router;
